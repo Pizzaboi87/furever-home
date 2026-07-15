3399bffb7b5ed6b4fe1fcbb23cd7ae2589cf2627
Pizzaboi87 <peterweiser87@gmail.com>
2026-07-15 20:56:32 +0300

    added cron-job for demo date-change and inserted google analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Script from 'next/script'
 
 import AppToaster from '@/components/ui/AppToaster'
 import {
@@ -134,6 +135,20 @@ export default function RootLayout({
         />
         {children}
         <AppToaster />
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PWMM303M4E"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PWMM303M4E');
+          `}
+        </Script>
       </body>
     </html>
   )
